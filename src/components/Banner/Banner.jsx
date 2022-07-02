899143babb0c180851cc55d7240f11b6f0e8e8c5
McTechie <mc.soans02@gmail.com>
2022-07-02 19:54:13 +0530

    fix: image optimizations for banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,4 +1,4 @@
-import Image from 'next/image';
+import Image from 'next/image'
 import { Carousel } from 'react-responsive-carousel'
 import 'react-responsive-carousel/lib/styles/carousel.min.css'
 
@@ -15,13 +15,13 @@ const Banner = () => {
         interval={5000}
       >
         <div>
-          <img loading='lazy' src='/banner1.jpg' alt='' />
+          <Image loading='lazy' src='/banner1.webp' alt='Home Shopping Spree' width={1536} height={630} />
         </div>
         <div>
-          <img loading='lazy' src='/banner2.jpg' alt='' />
+          <Image loading='lazy' src='/banner2.webp' alt='Buy Mobile Accessories' width={1536} height={630} />
         </div>
         <div>
-          <img loading='lazy' src='/banner3.jpg' alt='' />
+          <Image loading='lazy' src='/banner3.webp' alt='Content Creator Accessories' width={1536} height={630} />
         </div>
       </Carousel>
     </div>
