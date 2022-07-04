c4856a2ee514aed5daa6bddab36355cb304220fc
McTechie <mc.soans02@gmail.com>
2022-07-04 11:03:03 +0530

    fix: nextauth server error

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,11 +1,11 @@
 import NextAuth from 'next-auth/next'
-import Google from 'next-auth/providers/google'
+import GoogleProvider from 'next-auth/providers/google'
 
 export default NextAuth({
   providers: [
-    Google({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
 })
